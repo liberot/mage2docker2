0d537d23b336f97e60fc509ee6a463be071347f4
vberzsin <vberzsin@web.de>
2020-08-13 23:10:30 +0200

    initial mariadb fillup

--- htdocs/magento2/setup.sh
+++ htdocs/magento2/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "setup.sh"
 
-echo "about to execute composer wait.."
-/usr/bin/php -d memory_limit=-1 ./composer.phar upgrade 
-/usr/bin/php -d memory_limit=-1 ./composer.phar install
+#echo "about to execute composer wait.."
+#/usr/bin/php -d memory_limit=-1 ./composer.phar upgrade 
+#/usr/bin/php -d memory_limit=-1 ./composer.phar install
 
 echo "about to setup mage2 wait..."
-/usr/bin/php -d memory_limit=-1 ./bin/magento setup:install
 /usr/bin/php -d memory_limit=-1 ./bin/magento setup:upgrade
+/usr/bin/php -d memory_limit=-1 ./bin/magento setup:install
 /usr/bin/php -d memory_limit=-1 ./bin/magento setup:di:compile
 /usr/bin/php -d memory_limit=-1 ./bin/magento setup:static-content:deploy -f
 /usr/bin/php -d memory_limit=-1 ./bin/magento indexer:reindex
