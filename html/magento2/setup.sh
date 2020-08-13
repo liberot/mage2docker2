ae8683699fb06e04c9c3dcecaf8a8874f6c510b3
vberzsin <vberzsin@web.de>
2020-08-13 17:08:55 +0200

    nough

--- html/magento2/setup.sh
+++ html/magento2/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+echo "setup.sh"
+
 echo "about to execute composer wait.."
-echo "./app/ent/env.php 'dbhost' sometimes wonks with 'db' might maybe wait.."
 
 /usr/bin/php -d memory_limit=-1 ./composer.phar upgrade 
 /usr/bin/php -d memory_limit=-1 ./composer.phar install
 
-echo "about to setup mage2"
+echo "about to setup mage2 wait..."
 
 /usr/bin/php -d memory_limit=-1 ./bin/magento setup:install
 /usr/bin/php -d memory_limit=-1 ./bin/magento setup:upgrade
@@ -14,7 +15,6 @@ echo "about to setup mage2"
 /usr/bin/php -d memory_limit=-1 ./bin/magento setup:static-content:deploy -f
 /usr/bin/php -d memory_limit=-1 ./bin/magento indexer:reindex
 
-echo "about to start server"
+echo "about to start server.. now diss is..."
 /usr/bin/php -d memory_limit=-1 -S 127.0.0.1:80 -t ./pub ./phpserver/router.php
 
-echo "did"
