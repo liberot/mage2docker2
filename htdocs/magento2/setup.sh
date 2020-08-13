a8858771975a6043256d98a3bbf3961df1df02d4
vberzsin <vberzsin@web.de>
2020-08-14 00:19:01 +0200

    funkydub mage docker

--- htdocs/magento2/setup.sh
+++ htdocs/magento2/setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "setup.sh"
+#echo "setup.sh"
 
 #echo "about to execute composer wait.."
-/usr/bin/php -d memory_limit=-1 ./composer.phar upgrade 
-/usr/bin/php -d memory_limit=-1 ./composer.phar install
+#/usr/bin/php -d memory_limit=-1 ./composer.phar upgrade 
+#/usr/bin/php -d memory_limit=-1 ./composer.phar install
 
-echo "about to setup mage2 wait..."
-/usr/bin/php -d memory_limit=-1 ./bin/magento setup:upgrade
-/usr/bin/php -d memory_limit=-1 ./bin/magento setup:install
-/usr/bin/php -d memory_limit=-1 ./bin/magento setup:di:compile
-/usr/bin/php -d memory_limit=-1 ./bin/magento setup:static-content:deploy -f
-/usr/bin/php -d memory_limit=-1 ./bin/magento indexer:reindex
+#echo "about to setup mage2 wait..."
+#/usr/bin/php -d memory_limit=-1 ./bin/magento setup:upgrade
+#/usr/bin/php -d memory_limit=-1 ./bin/magento setup:install
+#/usr/bin/php -d memory_limit=-1 ./bin/magento setup:di:compile
+#/usr/bin/php -d memory_limit=-1 ./bin/magento setup:static-content:deploy -f
+#/usr/bin/php -d memory_limit=-1 ./bin/magento indexer:reindex
 
-echo "about to start server.. now diss is..."
-/usr/bin/php -S 127.0.0.1:8080 -t ./pub ./phpserver/router.php
+#echo "about to start server.. now diss is..."
+#/usr/bin/php -S 127.0.0.1:8080 -t ./pub ./phpserver/router.php
 
